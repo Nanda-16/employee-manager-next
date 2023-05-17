e9478f4afb527dce221585f0328d1241f00d71cb
Nanda-16 <nandanakrishna1601@gmail.com>
2023-05-17 12:50:55 +0530

    employee delete dummy data handling

--- app/home/employees/page.js
+++ app/home/employees/page.js
@@ -8,13 +8,14 @@ import Image from "next/image"
 import Table from "@/components/table"
 import { useState } from "react";
 import Modal from "@/components/modal";
+import Alert from "@/components/alert";
 
 export const metadata = {
   title: 'Employees - Employee Mananager',
   description: 'Employee Manangement application',
 }
 
-const employees = [
+const dummy = [
   { 'id': 11, 'name': 'Raisal', 'dob': 'Dec 01,1990', 'joining_date': 'Dec 19,2012', 'gender': 'Male', 'designation': 'Software Developer', 'email': 'raisal@gmail.com' },
   { 'id': 12, 'name': 'Diya', 'dob': 'Jan 12,1991', 'joining_date': 'Dec 20,2012', 'gender': 'Female', 'designation': 'UI/UX Designer', 'email': 'diya@gmail.com' },
   { 'id': 13, 'name': 'John', 'dob': 'Jun 21,1995', 'joining_date': 'Jan 09,2013', 'gender': 'Male', 'designation': 'Frontend Developer', 'email': 'john@gmail.com' },
@@ -24,18 +25,33 @@ const employees = [
 
 const tableHeads = ['SI.No', 'Profile Pic', 'Name', 'Join Date', 'DOB', 'Gender', 'Designation', 'Email Address', 'Action']
 
-function Employee() {
+export default function Employee() {
 
-  const [open, setOpen] = useState(false);
+  const [employees, setEmployees] = useState(dummy);
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [toast, setToast] = useState(false);
+  const [deleteEmployee, setDeleteEmployee] = useState("");
 
-  const modalClose = () => {
-    setOpen(false);
-  };
-
-  const openDeleteModal = (id) => {
-    console.log(id, 'deleteee');
-    setOpen(true);
+  const openDeleteModal = (employee) => {
+    console.log(employee, 'deleteee');
+    setDeleteModal(true);
+    setDeleteEmployee(employee);
   }
+
+  const handleDelete = () => {
+    const newEmployees = employees.filter((employee) => employee.id !== deleteEmployee?.id);
+    setEmployees(newEmployees)
+    closeDeleteModal();
+    setToast(true);
+    setTimeout(() => {
+      setToast(false);
+    }, 3000);
+  }
+
+  const closeDeleteModal = () => {
+    setDeleteModal(false);
+    setDeleteEmployee('');
+  };
 
 
   return (
@@ -51,6 +67,11 @@ function Employee() {
                   + Add
                 </Button>
               </Link>
+              {toast && (
+                <div>
+                  <Alert variant="success" title="Success" message="Operation successful" close />
+                </div>
+              )}
             </Card.Action>
           </Card.Header>
           <Card.Body>
@@ -80,7 +101,7 @@ function Employee() {
                         variant="icon-danger"
                         className="rounded p-1 rounded-l-none"
                         title="Remove Employee"
-                        onClick={() => openDeleteModal(employee?.id)}
+                        onClick={() => openDeleteModal(employee)}
                       >
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-5">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
@@ -94,31 +115,28 @@ function Employee() {
           </Card.Body>
         </Card>
 
-        <Modal show={open} onClose={setOpen}>
+        {/* delete modal */}
+        <Modal show={deleteModal} onClose={setDeleteModal}>
           <Modal.Content>
             <Modal.Header className="text-red-500">Warning</Modal.Header>
-            <Modal.Body className="text-gray-900">Do you really want to delete this employee ?</Modal.Body>
+            <Modal.Body className="text-gray-900">Do you really want to delete {deleteEmployee?.name} ?</Modal.Body>
           </Modal.Content>
           <Modal.Footer>
             <Button variant="danger"
               type="button"
-              onClick={modalClose}
+              onClick={handleDelete}
             >
               Delete
             </Button>
             <Button variant="secondary"
               type="button"
-              onClick={modalClose}
+              onClick={closeDeleteModal}
             >
               Cancel
             </Button>
           </Modal.Footer>
         </Modal>
-
-
       </Container>
     </>
   )
 }
-
-export default Employee
